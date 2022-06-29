4747e4d637e9e3381313bb0e761a98e3e848d86a
Your Name <samsulr99@gmail.com>
2022-06-29 11:13:15 +0700

    update

--- src/event.js
+++ src/event.js
@@ -2,10 +2,19 @@ import React from "react";
 
 function Event() {
   //Create function here
+  function Hello() {
+    return alert("Hallo Batch 36")
+  }
+
+  const name = 'Surya'
 
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <button onClick={Hello}>Klik Batch</button>
+      <button onClick={() => alert("Selamat Pagi")}>Klik Me</button>
+      <button onClick={() => alert(name)}>Klik Name</button>
+    </div>
   );
 }
 
