ab9fb97ed72b82741b253c6e55d2b69e0ecba795
Your Name <samsulr99@gmail.com>
2022-06-29 12:16:16 +0700

    update

--- src/state.js
+++ src/state.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 function State() {
   //init State
+  const [counter, setCounter] = useState(0)
+  console.log(counter);
 
   //Create Function Add
+  function Add(){
+   return setCounter(counter + 1)
+  }
 
   //Create Function Less
+  function Less(){
+
+    if(counter > 0){
+      return setCounter(counter - 1)
+    } 
+  }
 
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <h2>{counter}</h2>
+      <button onClick={Add}>Add Number</button>
+      <button onClick={Less}>Less Number</button>
+      <button onClick={() => setCounter(0)}>Reset</button>
+    </div>
   );
 }
 
