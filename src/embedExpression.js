86ccd03696265ac863284ebaed72bf553ade9a9c
Your Name <samsulr99@gmail.com>
2022-06-29 11:04:48 +0700

    upfate

--- src/embedExpression.js
+++ src/embedExpression.js
@@ -3,11 +3,21 @@ import React from "react";
 function EmbedExpression() {
   //Create function here
 
+  function getJumlah(a, b){
+    const jumlah = a + b
+
+    return jumlah
+  }
   //Create a variable here
+
+  const name = 'Samsul Rijal'
 
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <h2>{name}</h2>
+      <h2>{getJumlah(10, 20)}</h2>
+    </div>
   );
 }
 
